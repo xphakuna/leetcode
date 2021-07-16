3e81c7ec6875001fb10ab751fc655ba26ded2d50
xp <peng.xie@hlg.com>
2021-07-16 13:47:56 +0800

    add

---
+++ leetcode_compile/leetcode_compile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcode_compile
+{
+    class Program
+    {
+        int[][] parseParam(string str)
+        {
+            List<int[]> ret = new List<int[]>();
+            return ret.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ leetcode_compile/leetcode_compile/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace leetcode_compile
+{
+    class Utils
+    {
+        public static int FindBiggerThan<T>(IList<T> newp, int idx_start, int n, System.Func<T, int> calc)
+        {
+            int b = idx_start;
+            int e = newp.Count - 1;
+            while (b < e)
+            {
+                int m = (b + e) / 2;
+                int v = calc(newp[m]);
+                if (v < n) b = m + 1;
+                else if (v > n) e = m - 1;
+                else break;
+            }
+            for (int i = (b + e) / 2; i < newp.Count; i++)
+            {
+                if (calc(newp[i]) > n)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
